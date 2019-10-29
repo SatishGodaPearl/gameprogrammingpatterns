c8d95e03c696696273ec1dc0c8262953f05f21a1
Satish Goda <37197216+SatishGodaPearl@users.noreply.github.com>
2019-10-29 15:36:33 -0700

    Behavioral.TypeObject pattern

---
+++ Behavioral/Type_Object/__init__.py
@@ -0,0 +1,5 @@
+"""
+Allow the flexible creation of new “classes” by creating a single class, each instance of which represents a different type of object.
+
+https://gameprogrammingpatterns.com/type-object.html
+"""
